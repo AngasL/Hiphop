989c408fbae16913707f886a309317a02956254d
Angas Liu <LIUYX@legal.regn.net>
2017-08-21 18:09:25 +0800

    Update Hiphop Context.

--- Hiphop.Repository/HiphopContext.cs
+++ Hiphop.Repository/HiphopContext.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Hiphop.Repository.Model;
+using Microsoft.Extensions.Configuration;
 
 namespace Hiphop.Repository
 {
 	public class HiphopContext : DbContext
 	{
-		public HiphopContext(DbContextOptions<HiphopContext> options) 
+		private readonly IConfigurationRoot _config;
+		public HiphopContext(IConfigurationRoot config,
+			DbContextOptions<HiphopContext> options)
 			: base(options)
 		{
+			this._config = config;
 		}
 
 		public DbSet<User> Users { get; set; }
@@ -15,6 +19,12 @@ namespace Hiphop.Repository
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
 			modelBuilder.Entity<User>().ToTable("User");
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+			optionsBuilder.UseSqlServer(_config["ConnectinoStrings:Hiphop"]);
 		}
 	}
 }
